5e113fe3eaaa9a6ac3c00a0cbd426c2d7fadc3d2
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-23 12:59:34 -0300

    using map

--- app/main.js
+++ app/main.js
@@ -4,29 +4,11 @@ const endpointAPI =
 
 getBuscarLivrosDaAPI();
 
-const elementoParaInserirLivros = document.getElementById("livros");
-
 async function getBuscarLivrosDaAPI() {
-  const response = await fetch(endpointAPI);
-  livros = await response.json();
-  console.table(livros);
+  const res = await fetch(endpointAPI);
+  livros = await res.json();
 
-  exibirOsLivrosNaTela(livros);
-}
+  let livrosComDesconto = aplicarDesconto(livros);
 
-function exibirOsLivrosNaTela(listaDeLivros) {
-  listaDeLivros.forEach((livro) => {
-    elementoParaInserirLivros.innerHTML += `    <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-       ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
-`;
-  });
+  exibirOsLivrosNaTela(livrosComDesconto);
 }
