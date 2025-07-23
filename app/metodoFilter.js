03482cb9ab0eeee581d1aa5b69499d9cfddc950c
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-07-23 17:04:19 -0300

    changing html with js

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,6 +5,23 @@ botoes.forEach((btn) => btn.addEventListener("click", filtrarLivros));
 function filtrarLivros() {
   const elementoBtn = document.getElementById(this.id);
   const categoria = elementoBtn.value;
-  let livrosFiltrados = categoria == "disponivel" ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria);
+  let livrosFiltrados = categoria == "disponivel" ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
   exibirOsLivrosNaTela(livrosFiltrados);
+if(categoria == 'disponivel'){
+  exibirValorTotalDeLivrosDisponiveis()
+}
+} 
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter(livro => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDeLivrosDisponiveis(){
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `  <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">399,00</span></p>
+      </div>`
 }
